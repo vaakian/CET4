3d52cdcae1b8d058e924c9a41d915ac08953f84d
alva <861729091@qq.com>
2019-08-18 12:08:00 +0800

    支持CET6, 自动判断CET4/6级，年份

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask import Flask, request, render_template
 import requests
 
 class STUDENT:
-	session = requests.session()
-	session.headers = {'Referer': 'http://cache.neea.edu.cn/'}
+	
 	def __init__(self, num, name):
 		self.ver_url = 'http://cache.neea.edu.cn/Imgs.do?c=CET&ik=' + \
 			num + '&t=0.8030510069176611'
 		self.num = num
 		self.name = name
+		# 每个准考证应是单独的session
+		self.session = requests.Session()
+		self.session.headers = {'Referer': 'http://cache.neea.edu.cn/'}
 	def get_ver_img(self):
 		ver_code = self.session.get(self.ver_url)
 		ver_code = ver_code.text[13:-3]
@@ -17,7 +19,6 @@ class STUDENT:
 		return ver_img
 
 	def get_score(self, DANGCI, v):
-  		# http://cache.neea.edu.cn/cet/query?data=CET4_191_DANGCI,420640191107626,熊维建&v=yfp5
 		params = {
 			'data': '{},{},{}'.format(DANGCI, self.num, self.name),
 			'v': v
@@ -31,12 +32,12 @@ class STUDENT:
 app = Flask(__name__)
 api = Api(app)
 stu_list = {}
-
 class SCORE(Resource):
 	def get(self):	# 返回验证码, 此时创建对象
 		num = request.args.get('num')
 		name = request.args.get('name')
 		stu_list[num] = STUDENT(num, name)
+		print(stu_list)
 		stu = stu_list[num]
 		ver_img = stu.get_ver_img()
 		return {'url': ver_img}
@@ -45,12 +46,20 @@ class SCORE(Resource):
 		data = request.json
 		num = data['num']
 		name = data['name']
-		dangci = data['dangci']
+		# dangci = data['dangci']
+		dangci = ''
+		if num[9] == '1':
+  			dangci = 'CET4_{}_DANGCI'.format(num[6:9])
+		else:
+  			dangci = 'CET6_{}_DANGCI'.format(num[6:9])
+		print({"dangci": dangci})
 		v = data['v']
 		stu = stu_list[num]
 		score = stu.get_score(dangci, v)
 		del stu	# 释放对象
 		del stu_list[num]	# 删除记录
+
+		print(stu_list)
 		return score[:-2].replace('result.callback(', '')
 
 # 输入用户名账号-》请求验证码-》创建stu对象-》请求成绩使用已创建好的对象
@@ -61,4 +70,4 @@ api.add_resource(SCORE, '/score')
 def index():
   	return render_template('index.html')
 if __name__ == '__main__':
-	app.run(debug=False, port=86, host='0.0.0.0')
+	app.run(debug=True, port=86, host='0.0.0.0')
